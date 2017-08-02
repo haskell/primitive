f4239ec56230411c4be7850919dfcea116671198
Ben Gamari <ben@smart-cactus.org>
2017-08-02 13:44:07 -0400

    Add IsList ByteArray instance

--- Data/Primitive/ByteArray.hs
+++ Data/Primitive/ByteArray.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE CPP, MagicHash, UnboxedTuples, UnliftedFFITypes, DeriveDataTypeable #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 
 -- |
 -- Module      : Data.Primitive.ByteArray
@@ -38,11 +39,16 @@ module Data.Primitive.ByteArray (
 ) where
 
 import Control.Monad.Primitive
+import Control.Monad.ST
+import Control.Monad ( zipWithM_ )
 import Data.Primitive.Types
 
 import Foreign.C.Types
 import Data.Word ( Word8 )
 import GHC.Base ( Int(..) )
+#if __GLASGOW_HASKELL__ >= 708
+import qualified GHC.Exts as Exts ( IsList(..) )
+#endif
 import GHC.Prim
 #if __GLASGOW_HASKELL__ >= 706
     hiding (setByteArray#)
@@ -61,23 +67,23 @@ data ByteArray = ByteArray ByteArray# deriving ( Typeable )
 data MutableByteArray s = MutableByteArray (MutableByteArray# s)
                                         deriving( Typeable )
 
--- | Create a new mutable byte array of the specified size.
+-- | Create a new mutable byte array of the specified size in bytes.
 newByteArray :: PrimMonad m => Int -> m (MutableByteArray (PrimState m))
 {-# INLINE newByteArray #-}
 newByteArray (I# n#)
   = primitive (\s# -> case newByteArray# n# s# of
                         (# s'#, arr# #) -> (# s'#, MutableByteArray arr# #))
 
--- | Create a /pinned/ byte array of the specified size. The garbage collector
--- is guaranteed not to move it.
+-- | Create a /pinned/ byte array of the specified size in bytes. The garbage
+-- collector is guaranteed not to move it.
 newPinnedByteArray :: PrimMonad m => Int -> m (MutableByteArray (PrimState m))
 {-# INLINE newPinnedByteArray #-}
 newPinnedByteArray (I# n#)
   = primitive (\s# -> case newPinnedByteArray# n# s# of
                         (# s'#, arr# #) -> (# s'#, MutableByteArray arr# #))
 
--- | Create a /pinned/ byte array of the specified size and with the give
--- alignment. The garbage collector is guaranteed not to move it.
+-- | Create a /pinned/ byte array of the specified size in bytes and with the
+-- give alignment. The garbage collector is guaranteed not to move it.
 newAlignedPinnedByteArray
   :: PrimMonad m => Int -> Int -> m (MutableByteArray (PrimState m))
 {-# INLINE newAlignedPinnedByteArray #-}
@@ -163,6 +169,12 @@ foldrByteArray f z arr = go 0
       | sizeofByteArray arr > i * sz = f (indexByteArray arr i) (go (i+1))
       | otherwise                    = z
     sz = sizeofByteArray arr
+
+fromListN :: Prim a => Int -> [a] -> ByteArray
+fromListN n xs = runST $ do
+    marr <- newByteArray (n * sizeOf (head xs))
+    zipWithM_ (writeByteArray marr) [0..n] xs
+    unsafeFreezeByteArray marr
 
 #if __GLASGOW_HASKELL__ >= 702
 unI# :: Int -> Int#
@@ -291,6 +303,7 @@ instance Show ByteArray where
 
 foreign import ccall unsafe "primitive-memops.h hsprimitive_memcmp"
   memcmp_ba :: ByteArray# -> ByteArray# -> CSize -> IO CInt
+
 sameByteArray :: ByteArray# -> ByteArray# -> Bool
 sameByteArray ba1 ba2 =
     case reallyUnsafePtrEquality# (unsafeCoerce# ba1 :: ()) (unsafeCoerce# ba2 :: ()) of
@@ -300,7 +313,6 @@ sameByteArray ba1 ba2 =
       1# -> True
       0# -> False
 #endif
-
 
 instance Eq ByteArray where
   ba1@(ByteArray ba1#) == ba2@(ByteArray ba2#)
@@ -323,3 +335,12 @@ instance Ord ByteArray where
     where
       n1 = sizeofByteArray ba1
       n2 = sizeofByteArray ba2
+
+#if __GLASGOW_HASKELL__ >= 708
+instance Exts.IsList ByteArray where
+  type Item ByteArray = Word8
+
+  toList = foldrByteArray (:) []
+  fromList xs = fromListN (length xs) xs
+  fromListN = fromListN
+#endif
