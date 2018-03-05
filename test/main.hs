68f6d910a3015e4f14ac4f479abccc40f96cdaa7
Andrew Martin <andrew.thaddeus@gmail.com>
2018-03-05 14:15:03 -0500

    add tests for ByteArray Monoid and Semigroup instances

--- test/main.hs
+++ test/main.hs
@@ -3,10 +3,12 @@
 import Control.Monad
 import Control.Monad.Primitive
 import Control.Monad.ST
+import Data.Monoid
 import Data.Primitive
 import Data.Primitive.Array
 import Data.Primitive.ByteArray
 import Data.Primitive.Types
+import Data.Semigroup (stimes)
 import Data.Word
 import GHC.Int
 import GHC.IO
@@ -41,12 +43,22 @@ testByteArray = do
     let arr1 = mkByteArray ([0xde, 0xad, 0xbe, 0xef] :: [Word8])
         arr2 = mkByteArray ([0xde, 0xad, 0xbe, 0xef] :: [Word8])
         arr3 = mkByteArray ([0xde, 0xad, 0xbe, 0xee] :: [Word8])
+        arr4 = mkByteArray ([0xde, 0xad, 0xbe, 0xdd] :: [Word8])
+        arr5 = mkByteArray ([0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xdd] :: [Word8])
     when (show arr1 /= "[0xde, 0xad, 0xbe, 0xef]") $
         fail $ "ByteArray Show incorrect: "++show arr1
     unless (arr1 > arr3) $
         fail $ "ByteArray Ord incorrect"
     unless (arr1 == arr2) $
         fail $ "ByteArray Eq incorrect"
+    unless (mappend arr1 arr4 == arr5) $
+        fail $ "ByteArray Monoid mappend incorrect"
+    unless (mappend arr1 (mappend arr3 arr4) == mappend (mappend arr1 arr3) arr4) $
+        fail $ "ByteArray Monoid mappend not associative"
+    unless (mconcat [arr1,arr2,arr3,arr4,arr5] == (arr1 <> arr2 <> arr3 <> arr4 <> arr5)) $
+        fail $ "ByteArray Monoid mconcat incorrect"
+    unless (stimes (3 :: Int) arr4 == (arr4 <> arr4 <> arr4)) $
+        fail $ "ByteArray Semigroup stimes incorrect"
 
 mkByteArray :: Prim a => [a] -> ByteArray
 mkByteArray xs = runST $ do
