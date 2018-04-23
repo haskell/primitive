302d5b951a4339b06a195e7ea663ee9de5566b4c
Andrew Martin <andrew.thaddeus@gmail.com>
2018-04-23 08:11:17 -0400

    document reason for specialized either traversal in benchmark suite

--- bench/Array/Traverse/Either.hs
+++ bench/Array/Traverse/Either.hs
@@ -9,6 +9,13 @@ import Control.Monad.Trans.State.Strict
 import Control.Monad.Primitive
 import Data.Primitive.Array
 
+-- This is a specialization of traverse, where the applicative is
+-- chosen to be Either. In the benchmark suite, this implementation
+-- is compared against an implementation that uses ExceptT to see
+-- if GHC is able to optimize the ExceptT variant to code as efficient
+-- as this. At the time this test was written (2018-04-23), GHC does
+-- appear to optimize the ExceptT variant so that it performs as well
+-- as this one.
 {-# INLINE traverseEither #-}
 traverseEither ::
      (a -> Either e b)
