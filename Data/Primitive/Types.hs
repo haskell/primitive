68322d75bfc538875a35aabbe5ce965258749772
chessai <chessai1996@gmail.com>
2018-07-06 12:56:30 -0400

    add since pragma to Complex Prim instance. remove redundant +# 0#

--- Data/Primitive/Types.hs
+++ Data/Primitive/Types.hs
@@ -142,32 +142,33 @@ sizeOf x = I# (sizeOf# x)
 alignment :: Prim a => a -> Int
 alignment x = I# (alignment# x)
 
+-- | @since 0.6.4.1
 instance Prim a => Prim (Complex a) where
   sizeOf# _ = 2# *# sizeOf# (undefined :: a)
   alignment# _ = alignment# (undefined :: a)
   indexByteArray# arr# i# =
-    let x = indexByteArray# arr# (2# *# i# +# 0#)
+    let x = indexByteArray# arr# (2# *# i#)
         y = indexByteArray# arr# (2# *# i# +# 1#)
     in x :+ y
   readByteArray# arr# i# =
-    \s0 -> case readByteArray# arr# (2# *# i# +# 0#) s0 of
+    \s0 -> case readByteArray# arr# (2# *# i#) s0 of
        (# s1#, x #) -> case readByteArray# arr# (2# *# i# +# 1#) s1# of
           (# s2#, y #) -> (# s2#, x :+ y #)
   writeByteArray# arr# i# (a :+ b) =
-    \s0 -> case writeByteArray# arr# (2# *# i# +# 0#) a s0 of
+    \s0 -> case writeByteArray# arr# (2# *# i#) a s0 of
        s1 -> case writeByteArray# arr# (2# *# i# +# 1#) b s1 of
          s2 -> s2
   setByteArray# = defaultSetByteArray#
   indexOffAddr# addr# i# =
-    let x = indexOffAddr# addr# (2# *# i# +# 0#)
+    let x = indexOffAddr# addr# (2# *# i#)
         y = indexOffAddr# addr# (2# *# i# +# 1#)
     in x :+ y
   readOffAddr# addr# i# =
-    \s0 -> case readOffAddr# addr# (2# *# i# +# 0#) s0 of
+    \s0 -> case readOffAddr# addr# (2# *# i#) s0 of
        (# s1, x #) -> case readOffAddr# addr# (2# *# i# +# 1#) s1 of
          (# s2, y #) -> (# s2, x :+ y #)
   writeOffAddr# addr# i# (a :+ b) =
-    \s0 -> case writeOffAddr# addr# (2# *# i# +# 0#) a s0 of
+    \s0 -> case writeOffAddr# addr# (2# *# i#) a s0 of
        s1 -> case writeOffAddr# addr# (2# *# i# +# 1#) b s1 of
          s2 -> s2
   setOffAddr# = defaultSetOffAddr#
