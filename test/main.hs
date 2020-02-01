f8e710cab0ec13f7a26af31f3751ab8985f29c22
Andrew Martin <andrew.thaddeus@gmail.com>
2020-02-01 11:45:39 -0500

    Depend on quickcheck-classes-base rather than vendoring quickcheck-classes

--- test/main.hs
+++ test/main.hs
@@ -56,8 +56,8 @@ import Test.Tasty (defaultMain,testGroup,TestTree)
 import Test.QuickCheck (Arbitrary,Arbitrary1,Gen,(===),CoArbitrary,Function)
 import qualified Test.Tasty.QuickCheck as TQC
 import qualified Test.QuickCheck as QC
-import qualified Test.QuickCheck.Classes as QCC
-import qualified Test.QuickCheck.Classes.IsList as QCCL
+import qualified Test.QuickCheck.Classes.Base as QCC
+import qualified Test.QuickCheck.Classes.Base.IsList as QCCL
 import qualified Data.List as L
 
 main :: IO ()
