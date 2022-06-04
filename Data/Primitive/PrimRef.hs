eeb0a5996bdb123dd39379b494f93a30dc983f6d
Brian Shu <littlebubu.shu@gmail.com>
2022-06-04 14:14:58 -0400

    change name to PrimVar and remove FrozenPrimRef

--- Data/Primitive/PrimRef.hs
+++ Data/Primitive/PrimRef.hs
@@ -5,24 +5,17 @@
 {-# LANGUAGE Unsafe #-}
 
 -- | Note: Edward Kmett wrote everything in this module. 
-module Data.Primitive.PrimRef
+module Data.Primitive.PrimVar
   ( 
   -- * Primitive References
-    PrimRef(..)
-  , newPrimRef
-  , newPinnedPrimRef
-  , newAlignedPinnedPrimRef
-  , readPrimRef
-  , writePrimRef
-  , primRefContents
-  -- * Frozen Primitive References
-  , FrozenPrimRef(..)
-  , newFrozenPrimRef
-  , unsafeFreezePrimRef
-  , unsafeThawPrimRef
-  , indexFrozenPrimRef
-  , frozenPrimRefContents
-  -- * Atomic Operations
+    PrimVar(..)
+  , newPrimVar
+  , newPinnedPrimVar
+  , newAlignedPinnedPrimVar
+  , readPrimVar
+  , writePrimVar
+  , primVarContents
+  , primVarToMutablePrimArray
   , casInt
   , fetchAddInt
   , fetchSubInt
@@ -35,8 +28,6 @@ module Data.Primitive.PrimRef
   ) where
 
 import Control.Monad.Primitive
-import Control.Monad.ST
-import Data.Data
 import Data.Primitive
 import GHC.Exts
 import GHC.Ptr (castPtr)
@@ -45,153 +36,111 @@ import GHC.Ptr (castPtr)
 -- * Primitive References
 --------------------------------------------------------------------------------
 
-newtype PrimRef s a = PrimRef (MutablePrimArray s a)
+newtype PrimVar s a = PrimVar (MutablePrimArray s a)
 
-type role PrimRef nominal nominal
+type role PrimVar nominal nominal
 
 -- | Create a primitive reference.
-newPrimRef :: (PrimMonad m, Prim a) => a -> m (PrimRef (PrimState m) a)
-newPrimRef a = do
+newPrimVar :: (PrimMonad m, Prim a) => a -> m (PrimVar (PrimState m) a)
+newPrimVar a = do
   m <- newPrimArray 1
   writePrimArray m 0 a
-  return (PrimRef m)
-{-# INLINE newPrimRef #-}
+  return (PrimVar m)
+{-# INLINE newPrimVar #-}
 
 -- | Create a pinned primitive reference.
-newPinnedPrimRef :: (PrimMonad m, Prim a) => a -> m (PrimRef (PrimState m) a)
-newPinnedPrimRef a = do
+newPinnedPrimVar :: (PrimMonad m, Prim a) => a -> m (PrimVar (PrimState m) a)
+newPinnedPrimVar a = do
   m <- newPinnedPrimArray 1
   writePrimArray m 0 a
-  return (PrimRef m)
-{-# INLINE newPinnedPrimRef #-}
+  return (PrimVar m)
+{-# INLINE newPinnedPrimVar #-}
 
 -- | Create a pinned primitive reference with the appropriate alignment for its contents.
-newAlignedPinnedPrimRef :: (PrimMonad m, Prim a) => a -> m (PrimRef (PrimState m) a)
-newAlignedPinnedPrimRef a = do
+newAlignedPinnedPrimVar :: (PrimMonad m, Prim a) => a -> m (PrimVar (PrimState m) a)
+newAlignedPinnedPrimVar a = do
   m <- newAlignedPinnedPrimArray 1
   writePrimArray m 0 a
-  return (PrimRef m)
-{-# INLINE newAlignedPinnedPrimRef #-}
+  return (PrimVar m)
+{-# INLINE newAlignedPinnedPrimVar #-}
 
 -- | Read a primitive value from the reference
-readPrimRef :: (PrimMonad m, Prim a) => PrimRef (PrimState m) a -> m a
-readPrimRef (PrimRef m) = readPrimArray m 0
-{-# INLINE readPrimRef #-}
+readPrimVar :: (PrimMonad m, Prim a) => PrimVar (PrimState m) a -> m a
+readPrimVar (PrimVar m) = readPrimArray m 0
+{-# INLINE readPrimVar #-}
 
 -- | Write a primitive value to the reference
-writePrimRef :: (PrimMonad m, Prim a) => PrimRef (PrimState m) a -> a -> m ()
-writePrimRef (PrimRef m) a = writePrimArray m 0 a
-{-# INLINE writePrimRef #-}
+writePrimVar :: (PrimMonad m, Prim a) => PrimVar (PrimState m) a -> a -> m ()
+writePrimVar (PrimVar m) a = writePrimArray m 0 a
+{-# INLINE writePrimVar #-}
 
-instance Eq (PrimRef s a) where
-  PrimRef m == PrimRef n = sameMutablePrimArray m n
+instance Eq (PrimVar s a) where
+  PrimVar m == PrimVar n = sameMutablePrimArray m n
   {-# INLINE (==) #-}
 
--- | Yield a pointer to the data of a 'PrimRef'. This operation is only safe on pinned byte arrays allocated by
--- 'newPinnedPrimRef' or 'newAlignedPinnedPrimRef'.
-primRefContents :: PrimRef s a -> Ptr a
-primRefContents (PrimRef m) = castPtr $ mutablePrimArrayContents m
-{-# INLINE primRefContents #-}
+-- | Yield a pointer to the data of a 'PrimVar'. This operation is only safe on pinned byte arrays allocated by
+-- 'newPinnedPrimVar' or 'newAlignedPinnedPrimVar'.
+primVarContents :: PrimVar s a -> Ptr a
+primVarContents (PrimVar m) = castPtr $ mutablePrimArrayContents m
+{-# INLINE primVarContents #-}
 
---------------------------------------------------------------------------------
--- * Frozen Primitive References
---------------------------------------------------------------------------------
-
--- | Convert a mutable 'PrimRef' to an immutable one without copying. The reference should not be modified after the conversion.
-unsafeFreezePrimRef :: PrimMonad m => PrimRef (PrimState m) a -> m (FrozenPrimRef a)
-unsafeFreezePrimRef (PrimRef m) = FrozenPrimRef <$> unsafeFreezePrimArray m
-{-# INLINE unsafeFreezePrimRef #-}
-
-newtype FrozenPrimRef a = FrozenPrimRef (PrimArray a)
-
-type role FrozenPrimRef nominal
-
-newFrozenPrimRef :: Prim a => a -> FrozenPrimRef a
-newFrozenPrimRef a = runST $ newPrimRef a >>= unsafeFreezePrimRef
-
--- | Read the stored primitive value from the frozen reference.
-indexFrozenPrimRef :: Prim a => FrozenPrimRef a -> a
-indexFrozenPrimRef (FrozenPrimRef ba) = indexPrimArray ba 0
-{-# INLINE indexFrozenPrimRef #-}
-
--- | Convert an immutable primitive reference to a mutable one without copying. The original reference should not be used after the conversion.
-unsafeThawPrimRef :: PrimMonad m => FrozenPrimRef a -> m (PrimRef (PrimState m) a)
-unsafeThawPrimRef (FrozenPrimRef m) = PrimRef <$> unsafeThawPrimArray m
-{-# INLINE unsafeThawPrimRef #-}
-
--- | Yield a pointer to the data of a 'FrozenPrimRef'. This operation is only safe on pinned byte arrays allocated by
--- 'newPinnedPrimRef' or 'newAlignedPinnedPrimRef' and then subsequently frozen.
-frozenPrimRefContents :: FrozenPrimRef a -> Ptr a
-frozenPrimRefContents (FrozenPrimRef m) = castPtr $ primArrayContents m
-{-# INLINE frozenPrimRefContents #-}
+primVarToMutablePrimArray :: PrimVar s a -> MutablePrimArray s a
+primVarToMutablePrimArray (PrimVar m) = m
+{-# INLINE primVarToMutablePrimArray #-}
 
 --------------------------------------------------------------------------------
 -- * Atomic Operations
 --------------------------------------------------------------------------------
 
 -- | Given a primitive reference, the expected old value, and the new value, perform an atomic compare and swap i.e. write the new value if the current value matches the provided old value. Returns the value of the element before the operation. Implies a full memory barrier.
-casInt :: PrimMonad m => PrimRef (PrimState m) Int -> Int -> Int -> m Int
-casInt (PrimRef (MutablePrimArray m)) (I# old) (I# new) = primitive $ \s -> case casIntArray# m 0# old new s of
+casInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> Int -> m Int
+casInt (PrimVar (MutablePrimArray m)) (I# old) (I# new) = primitive $ \s -> case casIntArray# m 0# old new s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE casInt #-}
 
 -- | Given a reference, and a value to add, atomically add the value to the element. Returns the value of the element before the operation. Implies a full memory barrier.
-fetchAddInt :: PrimMonad m => PrimRef (PrimState m) Int -> Int -> m Int
-fetchAddInt (PrimRef (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchAddIntArray# m 0# x s of
+fetchAddInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m Int
+fetchAddInt (PrimVar (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchAddIntArray# m 0# x s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE fetchAddInt #-}
 
 -- | Given a reference, and a value to subtract, atomically subtract the value from the element. Returns the value of the element before the operation. Implies a full memory barrier.
-fetchSubInt :: PrimMonad m => PrimRef (PrimState m) Int -> Int -> m Int
-fetchSubInt (PrimRef (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchSubIntArray# m 0# x s of
+fetchSubInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m Int
+fetchSubInt (PrimVar (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchSubIntArray# m 0# x s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE fetchSubInt #-}
 
 -- | Given a reference, and a value to bitwise and, atomically and the value with the element. Returns the value of the element before the operation. Implies a full memory barrier.
-fetchAndInt :: PrimMonad m => PrimRef (PrimState m) Int -> Int -> m Int
-fetchAndInt (PrimRef (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchAndIntArray# m 0# x s of
+fetchAndInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m Int
+fetchAndInt (PrimVar (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchAndIntArray# m 0# x s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE fetchAndInt #-}
 
 -- | Given a reference, and a value to bitwise nand, atomically nand the value with the element. Returns the value of the element before the operation. Implies a full memory barrier.
-fetchNandInt :: PrimMonad m => PrimRef (PrimState m) Int -> Int -> m Int
-fetchNandInt (PrimRef (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchNandIntArray# m 0# x s of
+fetchNandInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m Int
+fetchNandInt (PrimVar (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchNandIntArray# m 0# x s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE fetchNandInt #-}
 
 -- | Given a reference, and a value to bitwise or, atomically or the value with the element. Returns the value of the element before the operation. Implies a full memory barrier.
-fetchOrInt :: PrimMonad m => PrimRef (PrimState m) Int -> Int -> m Int
-fetchOrInt (PrimRef (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchOrIntArray# m 0# x s of
+fetchOrInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m Int
+fetchOrInt (PrimVar (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchOrIntArray# m 0# x s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE fetchOrInt #-}
 
 -- | Given a reference, and a value to bitwise xor, atomically xor the value with the element. Returns the value of the element before the operation. Implies a full memory barrier.
-fetchXorInt :: PrimMonad m => PrimRef (PrimState m) Int -> Int -> m Int
-fetchXorInt (PrimRef (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchXorIntArray# m 0# x s of
+fetchXorInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m Int
+fetchXorInt (PrimVar (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchXorIntArray# m 0# x s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE fetchXorInt #-}
 
 -- | Given a reference, read an element. Implies a full memory barrier.
-atomicReadInt :: PrimMonad m => PrimRef (PrimState m) Int -> m Int
-atomicReadInt (PrimRef (MutablePrimArray m)) = primitive $ \s -> case atomicReadIntArray# m 0# s of
+atomicReadInt :: PrimMonad m => PrimVar (PrimState m) Int -> m Int
+atomicReadInt (PrimVar (MutablePrimArray m)) = primitive $ \s -> case atomicReadIntArray# m 0# s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE atomicReadInt #-}
 
 -- | Given a reference, write an element. Implies a full memory barrier.
-atomicWriteInt :: PrimMonad m => PrimRef (PrimState m) Int -> Int -> m ()
-atomicWriteInt (PrimRef (MutablePrimArray m)) (I# x) = primitive_ $ \s -> atomicWriteIntArray# m 0# x s
+atomicWriteInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m ()
+atomicWriteInt (PrimVar (MutablePrimArray m)) (I# x) = primitive_ $ \s -> atomicWriteIntArray# m 0# x s
 {-# INLINE atomicWriteInt #-}
-
-instance (Prim a, Data a) => Data (FrozenPrimRef a) where
-  gfoldl f z m   = z newFrozenPrimRef `f` indexFrozenPrimRef m
-  toConstr _     = newFrozenPrimRefConstr
-  gunfold k z c  = case constrIndex c of
-    1 -> k (z newFrozenPrimRef)
-    _ -> error "gunfold"
-  dataTypeOf _   = frozenPrimRefDataType
-
-newFrozenPrimRefConstr :: Constr
-newFrozenPrimRefConstr = mkConstr frozenPrimRefDataType "newFrozenPrimRef" [] Prefix
-
-frozenPrimRefDataType :: DataType
-frozenPrimRefDataType = mkDataType "Data.Transient.Primitive.FrozenPrimRef" [newFrozenPrimRefConstr]
