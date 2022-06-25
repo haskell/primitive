3b7940e051b1c88c942b89bbd0e4f8f5bf7df00d
Andrew Martin <andrew.thaddeus@gmail.com>
2022-06-25 11:46:14 -0400

    More documentation tweaks

--- Data/Primitive/PrimVar.hs
+++ Data/Primitive/PrimVar.hs
@@ -44,6 +44,7 @@ import GHC.Ptr (castPtr)
 -- * Primitive References
 --------------------------------------------------------------------------------
 
+-- | A 'PrimVar' behaves like a single-element mutable primitive array.
 newtype PrimVar s a = PrimVar (MutablePrimArray s a)
 
 type role PrimVar nominal nominal
@@ -72,12 +73,12 @@ newAlignedPinnedPrimVar a = do
   return (PrimVar m)
 {-# INLINE newAlignedPinnedPrimVar #-}
 
--- | Read a primitive value from the reference
+-- | Read a value from the 'PrimVar'.
 readPrimVar :: (PrimMonad m, Prim a) => PrimVar (PrimState m) a -> m a
 readPrimVar (PrimVar m) = readPrimArray m 0
 {-# INLINE readPrimVar #-}
 
--- | Write a primitive value to the reference
+-- | Write a value to the 'PrimVar'.
 writePrimVar :: (PrimMonad m, Prim a) => PrimVar (PrimState m) a -> a -> m ()
 writePrimVar (PrimVar m) a = writePrimArray m 0 a
 {-# INLINE writePrimVar #-}
@@ -101,25 +102,28 @@ primVarToMutablePrimArray (PrimVar m) = m
 -- * Atomic Operations
 --------------------------------------------------------------------------------
 
--- | Given a primitive reference, the expected old value, and the new value, perform an atomic compare and swap i.e. write the new value if the current value matches the provided old value. Returns the value of the element before the operation. Implies a full memory barrier.
+-- $atomic
+-- Atomic operations on `PrimVar s Int`. All atomic operations imply a full memory barrier.
+
+-- | Given a primitive reference, the expected old value, and the new value, perform an atomic compare and swap i.e. write the new value if the current value matches the provided old value. Returns the value of the element before the operation.
 casInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> Int -> m Int
 casInt (PrimVar (MutablePrimArray m)) (I# old) (I# new) = primitive $ \s -> case casIntArray# m 0# old new s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE casInt #-}
 
--- | Given a reference, and a value to add, atomically add the value to the element. Returns the value of the element before the operation. Implies a full memory barrier.
+-- | Given a reference, and a value to add, atomically add the value to the element. Returns the value of the element before the operation.
 fetchAddInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m Int
 fetchAddInt (PrimVar (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchAddIntArray# m 0# x s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE fetchAddInt #-}
 
--- | Given a reference, and a value to subtract, atomically subtract the value from the element. Returns the value of the element before the operation. Implies a full memory barrier.
+-- | Given a reference, and a value to subtract, atomically subtract the value from the element. Returns the value of the element before the operation.
 fetchSubInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m Int
 fetchSubInt (PrimVar (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchSubIntArray# m 0# x s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE fetchSubInt #-}
 
--- | Given a reference, and a value to bitwise and, atomically and the value with the element. Returns the value of the element before the operation. Implies a full memory barrier.
+-- | Given a reference, and a value to bitwise and, atomically and the value with the element. Returns the value of the element before the operation.
 fetchAndInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m Int
 fetchAndInt (PrimVar (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchAndIntArray# m 0# x s of
   (# s', result #) -> (# s', I# result #)
@@ -131,25 +135,25 @@ fetchNandInt (PrimVar (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetc
   (# s', result #) -> (# s', I# result #)
 {-# INLINE fetchNandInt #-}
 
--- | Given a reference, and a value to bitwise or, atomically or the value with the element. Returns the value of the element before the operation. Implies a full memory barrier.
+-- | Given a reference, and a value to bitwise or, atomically or the value with the element. Returns the value of the element before the operation.
 fetchOrInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m Int
 fetchOrInt (PrimVar (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchOrIntArray# m 0# x s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE fetchOrInt #-}
 
--- | Given a reference, and a value to bitwise xor, atomically xor the value with the element. Returns the value of the element before the operation. Implies a full memory barrier.
+-- | Given a reference, and a value to bitwise xor, atomically xor the value with the element. Returns the value of the element before the operation.
 fetchXorInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m Int
 fetchXorInt (PrimVar (MutablePrimArray m)) (I# x) = primitive $ \s -> case fetchXorIntArray# m 0# x s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE fetchXorInt #-}
 
--- | Given a reference, read an element. Implies a full memory barrier.
+-- | Given a reference, atomically read an element.
 atomicReadInt :: PrimMonad m => PrimVar (PrimState m) Int -> m Int
 atomicReadInt (PrimVar (MutablePrimArray m)) = primitive $ \s -> case atomicReadIntArray# m 0# s of
   (# s', result #) -> (# s', I# result #)
 {-# INLINE atomicReadInt #-}
 
--- | Given a reference, write an element. Implies a full memory barrier.
+-- | Given a reference, atomically write an element.
 atomicWriteInt :: PrimMonad m => PrimVar (PrimState m) Int -> Int -> m ()
 atomicWriteInt (PrimVar (MutablePrimArray m)) (I# x) = primitive_ $ \s -> atomicWriteIntArray# m 0# x s
 {-# INLINE atomicWriteInt #-}
